05895a26cd42cd528e463e582853e5c73af35ec0
Nick Strayer <nick.strayer@gmail.com>
2019-02-26 11:05:57 -0600

    New intro with text is working!

--- javascript/helpers.js
+++ javascript/helpers.js
@@ -1,15 +1,8 @@
-/**
- * Given a set of points, lay them out in a phyllotaxis layout.
- * Mutates the `points` passed in by updating the x and y values.
- *
- * @param {Object[]} points The array of points to update. Will get `x` and `y` set.
- * @param {Number} pointWidth The size in pixels of the point's width. Should also include margin.
- * @param {Number} xOffset The x offset to apply to all points
- * @param {Number} yOffset The y offset to apply to all points
- *
- * @return {Object[]} points with modified x and y
- */
 function phyllotaxisLayout(points, pointWidth, xOffset = 0, yOffset = 0, iOffset = 0) {
+  
+  const colorScale = d3.scaleSequential(d3.interpolateInferno)
+    .domain([0,points.length]);
+ 
   // theta determines the spiral of the layout
   const theta = Math.PI * (3 - Math.sqrt(5));
 
@@ -22,21 +15,55 @@ function phyllotaxisLayout(points, pointWidth, xOffset = 0, yOffset = 0, iOffset
 
     point.x = xOffset + phylloX - pointRadius;
     point.y = yOffset + phylloY - pointRadius;
+    point.color = colorToVec3(colorScale(i));
   });
 
   return points;
 }
 
-/**
- * Given a set of points, lay them out in a grid.
- * Mutates the `points` passed in by updating the x and y values.
- *
- * @param {Object[]} points The array of points to update. Will get `x` and `y` set.
- * @param {Number} pointWidth The size in pixels of the point's width. Should also include margin.
- * @param {Number} gridWidth The width of the grid of points
- *
- * @return {Object[]} points with modified x and y
- */
+
+function setupPassedDataLayout(data, width, height) {
+  const hasClassInfo = data[0]['class'] ? true:false;
+  let setColor;
+  if(hasClassInfo){
+    const unique_classes = unique(data, 'class');
+    const color_index = unique_classes.reduce((class_obj, cur_class, i) => {
+      class_obj[cur_class] = i;
+      return class_obj;
+    }, {});
+    
+    const colorScale = d3.scaleSequential(d3.interpolatePlasma)
+      .domain([0,unique_classes.length]);
+    
+    setColor = d => colorToVec3(colorScale(color_index[d['class']]));
+    
+  } else {
+    setColor = d => [0,0,0];
+  }
+ 
+  
+  // Object for retrieving the position and color of the original passed data
+  const mapToOriginal = data.reduce((id_to_vals, d, i) => {
+    id_to_vals[i] = {
+      x: d.x*width,
+      y: d.y*height,
+      color: setColor(d),
+    };
+    return id_to_vals;
+  }, {});
+
+  return points => {
+    points.forEach(d => {
+      const {x,y,color} = mapToOriginal[d.id];
+      d.x = x;
+      d.y = y;
+      d.color = color;
+    });
+    return points;
+  }
+}
+
+
 function gridLayout(points, pointWidth, gridWidth) {
   const pointHeight = pointWidth;
   const pointsPerRow = Math.floor(gridWidth / pointWidth);
@@ -50,92 +77,28 @@ function gridLayout(points, pointWidth, gridWidth) {
   return points;
 }
 
-/**
- * Given a set of points, lay them out randomly.
- * Mutates the `points` passed in by updating the x and y values.
- *
- * @param {Object[]} points The array of points to update. Will get `x` and `y` set.
- * @param {Number} pointWidth The size in pixels of the point's width. Should also include margin.
- * @param {Number} width The width of the area to place them in
- * @param {Number} height The height of the area to place them in
- *
- * @return {Object[]} points with modified x and y
- */
+
 function randomLayout(points, pointWidth, width, height) {
+  const colorScale = d3.scaleSequential(d3.interpolateSpectral)
+    .domain([0,points.length]);
+  
   const rnorm = d3.randomNormal(0,0.15);
     
-  points.forEach(d => {    
+  points.forEach((d,i) => {    
     d.x = rnorm() * width + width/2;
     d.y = rnorm() * height + height/2;
+    d.color = colorToVec3(colorScale(i));
   });
 
   return points;
 }
 
-/**
- * Given a set of points, lay them out in a sine wave.
- * Mutates the `points` passed in by updating the x and y values.
- *
- * @param {Object[]} points The array of points to update. Will get `x` and `y` set.
- * @param {Number} pointWidth The size in pixels of the point's width. Should also include margin.
- * @param {Number} width The width of the area to place them in
- * @param {Number} height The height of the area to place them in
- *
- * @return {Object[]} points with modified x and y
- */
-function sineLayout(points, pointWidth, width, height) {
-  const amplitude = (0.1 + Math.random()) * (height / 2);
-  const yOffset = height / 2;
-  const periods = 3;
-  const yScale = d3.scaleLinear()
-    .domain([0, points.length - 1])
-    .range([0, periods * 2 * Math.PI]);
-
-  points.forEach((point, i) => {
-    point.x = (i / points.length) * (width - pointWidth);
-    point.y = amplitude * Math.sin(yScale(i)) + yOffset;
-  });
-
-  return points;
-}
-
-/**
- * Given a set of points, lay them out in a spiral.
- * Mutates the `points` passed in by updating the x and y values.
- *
- * @param {Object[]} points The array of points to update. Will get `x` and `y` set.
- * @param {Number} pointWidth The size in pixels of the point's width. Should also include margin.
- * @param {Number} width The width of the area to place them in
- * @param {Number} height The height of the area to place them in
- *
- * @return {Object[]} points with modified x and y
- */
-function spiralLayout(points, pointWidth, width, height) {
-  const amplitude = 0.3 * (height / 2);
-  const xOffset = width / 2;
-  const yOffset = height / 2;
-  const periods = 20;
-
-  const rScale = d3.scaleLinear()
-    .domain([0, points.length -1])
-    .range([0, Math.min(width / 2, height / 2) - pointWidth]);
-
-  const thetaScale = d3.scaleLinear()
-    .domain([0, points.length - 1])
-    .range([0, periods * 2 * Math.PI]);
-
-  points.forEach((point, i) => {
-    point.x = rScale(i) * Math.cos(thetaScale(i)) + xOffset;
-    point.y = rScale(i) * Math.sin(thetaScale(i)) + yOffset;
-  });
-
-  return points;
-}
 
 function colorToVec3(color){
   const rgb = d3.rgb(color);
   return [rgb.r / 255, rgb.g / 255, rgb.b / 255];
 }
+
 
 // wrap d3 color scales so they produce vec3s with values 0-1
 function wrapColorScale(scale) {
@@ -145,32 +108,12 @@ function wrapColorScale(scale) {
   };
 }
 
+
 // Simple function to get the unique values in an array
 function unique(data, key){ 
   return d3.set(data.map(d => d[key])).values();
 };
 
-function initializePoints(data, width, height) {
-  
-  const unique_classes = unique(data, 'class');
-  const color_index = unique_classes.reduce((class_obj, cur_class, i) => {
-    class_obj[cur_class] = i;
-    return class_obj;
-  }, {});
-  
-  const colorScale = d3.scaleSequential(d3.interpolateViridis)
-    .domain([0,unique_classes.length]);
-
-  data.forEach((d,i) => {
-    d.x = d.x*width;
-    d.y = d.y*height;
-    d.id = i;
-    d.color = colorScale(color_index[d['class']]);
-    d.colorEnd =  colorToVec3(d.color);
-  })
-
-  return data
-}
 
 /**
  * Generate an object array of `numPoints` length with unique IDs
@@ -182,22 +125,21 @@ function createPoints(numPoints, pointWidth, width, height) {
 
   const points = d3.range(numPoints).map(id => ({
     id,
-    color: colorScale(id),
+    color: colorToVec3(colorScale(id)),
     // points start in the middle of the screen
     x: width / 2,
     y: height / 2,
-    colorEnd: [0, 0, 0],
   }));
   return points
 }
 
 function setupNewTransition(points, layout, colorScale){
+  
   // make previous end the new beginning
   points.forEach((d,i) => {
     d.sx = d.x;
     d.sy = d.y;
-    d.colorStart = d.colorEnd;
-    d.colorEnd = colorScale(i / points.length);
+    d.colorStart = d.color;
   });
  
   // layout points
