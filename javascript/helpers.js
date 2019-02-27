eadb4e2234ebcd943c4e9cba14b26b10ceb6fade
Nick Strayer <nick.strayer@gmail.com>
2019-02-27 11:35:25 -0600

    Fixed bug caused by mutating the data object that broke viz on resize.

--- javascript/helpers.js
+++ javascript/helpers.js
@@ -122,3 +122,125 @@ function setupNewTransition(points, layout){
   // layout points
   layout(points);
 }
+
+const frag_shader = `
+// set the precision of floating point numbers
+precision highp float;
+
+// this value is populated by the vertex shader
+varying vec3 fragColor;
+
+void main() {
+  // gl_FragColor is a special variable that holds the color of a pixel
+  gl_FragColor = vec4(fragColor, 1);
+}`;
+
+const vert_shader = `
+// per vertex attributes
+attribute vec2 positionStart;
+attribute vec2 positionEnd;
+attribute float index;
+attribute vec3 colorStart;
+attribute vec3 color;
+
+// variables to send to the fragment shader
+varying vec3 fragColor;
+
+// values that are the same for all vertices
+uniform float pointWidth;
+uniform float stageWidth;
+uniform float stageHeight;
+uniform float elapsed;
+uniform float duration;
+uniform float delayByIndex;
+
+// helper function to transform from pixel space to normalized device coordinates (NDC)
+// in NDC (0,0) is the middle, (-1, 1) is the top left and (1, -1) is the bottom right.
+vec2 normalizeCoords(vec2 position) {
+  // read in the positions into x and y vars
+  float x = position[0];
+  float y = position[1];
+
+  return vec2(
+    2.0 * ((x / stageWidth) - 0.5),
+    // invert y since we think [0,0] is bottom left in pixel space
+    -(2.0 * ((y / stageHeight) - 0.5)));
+}
+
+// helper function to handle cubic easing (copied from d3 for consistency)
+// note there are pre-made easing functions available via glslify.
+float easeCubicInOut(float t) {
+  t *= 2.0;
+  t = (t <= 1.0 ? t * t * t : (t -= 2.0) * t * t + 2.0) / 2.0;
+  
+  if (t > 1.0) {
+    t = 1.0;
+  }
+  
+  return t;
+}
+
+void main() {
+  // update the size of a point based on the prop pointWidth
+  gl_PointSize = pointWidth;
+  
+  float delay = delayByIndex * index;
+  
+  // number between 0 and 1 indicating how far through the animation this
+  // vertex is.
+  float t;
+  
+  // drawing without animation, so show end state immediately
+  if (duration == 0.0) {
+    t = 1.0;
+  
+  // still delaying before animating
+  } else if (elapsed < delay) {
+    t = 0.0;
+  
+  // otherwise we are animating, so use cubic easing
+  } else {
+    t = easeCubicInOut((elapsed - delay) / duration);
+  }
+
+  // interpolate position
+  vec2 position = mix(positionStart, positionEnd, t);
+  
+  // interpolate and send color to the fragment shader
+  fragColor = mix(colorStart, color, t);
+  
+  // scale to normalized device coordinates
+  // gl_Position is a special variable that holds the position of a vertex
+  gl_Position = vec4(normalizeCoords(position), 0.0, 1.0);
+}`;
+
+function setupPointDrawer(regl){
+  // function to compile a draw points regl func
+  return function(points) {
+    const drawPoints = regl({
+      frag: frag_shader,
+      vert: vert_shader,
+      attributes: {
+        positionStart: points.map(d => [d.sx, d.sy]),
+        positionEnd: points.map(d => [d.x, d.y]),
+        colorStart: points.map(d => d.colorStart),
+        color: points.map(d => d.color),
+        index: d3.range(points.length),
+      },
+      uniforms: {
+        pointWidth: regl.prop('pointWidth'),
+        stageWidth: regl.prop('stageWidth'),
+        stageHeight: regl.prop('stageHeight'),
+        delayByIndex: regl.prop('delayByIndex'),
+        duration: regl.prop('duration'),
+
+        // time in milliseconds since the prop startTime (i.e. time elapsed)
+        elapsed: ({ time }, { startTime = 0 }) => (time - startTime) * 1000,
+      },
+      count: points.length,
+      primitive: 'points',
+    });
+
+    return drawPoints;
+  }
+}
